edafba952848d29c00232b1ee2502b4e00fd63e9
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-08 15:22:32 +0600

    ProductItem controller with Views
    Overriding convert and adding shared views for edit button creating model. Dropdown karma also.

--- Memberships/Areas/Admin/Extentions/ConverionExtensions.cs
+++ Memberships/Areas/Admin/Extentions/ConverionExtensions.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Web;
 using Memberships.Areas.Admin.Models;
 using Memberships.Entities;
 using Memberships.Models;
+using TransactionScope = System.Transactions.TransactionScope;
 
 namespace Memberships.Areas.Admin.Extentions
 {
@@ -56,6 +59,87 @@ namespace Memberships.Areas.Admin.Extentions
             model.ProductLinkTexts.Add(link);
 
             return model;
+        }
+
+        public static async Task<IEnumerable<ProductItemModel>> Convert(this IQueryable<ProductItem> productItems,
+            ApplicationDbContext db)
+        {
+            if (productItems.Count().Equals(0))
+                return new List<ProductItemModel>();
+
+
+            return await (from productItem in productItems
+                select new ProductItemModel
+                {
+                  ItemId = productItem.ItemId,
+                  ProductId = productItem.ProductId,
+                  ItemTitle =  db.Items.FirstOrDefault(
+                      it => it.Id.Equals(productItem.ItemId)).Title,
+                  ProductTitle =  db.Products.FirstOrDefault(
+                      pt => pt.Id.Equals(productItem.ProductId)).Title
+                }).ToListAsync();
+        }
+
+        public static async Task<ProductItemModel> Convert(this ProductItem productItem,
+            ApplicationDbContext db, bool addListData = true)
+        {
+            var model = new ProductItemModel
+            {
+                ItemId = productItem.ItemId,
+                ProductId = productItem.ProductId,
+                Items = addListData? await db.Items.ToListAsync() : null,
+                Products = addListData? await db.Products.ToListAsync():null,
+                ProductTitle = (await db.ProductTypes.FirstOrDefaultAsync(p => p.Id.Equals(productItem.ProductId)))?.Title,
+                ItemTitle = (await db.Items.FirstOrDefaultAsync(i => i.Id.Equals(productItem.ItemId)))?.Title
+            };
+
+            return model;
+        }
+
+        public static async Task<bool> CanChange(this ProductItem productItem, ApplicationDbContext db)
+        {
+            var oldPi = await db.ProductItems.CountAsync(pi => pi.ProductId.Equals(
+                                                                   productItem.OldProductId) &&
+                                                               pi.ItemId.Equals(productItem.OldItemId));
+
+            var newPi = await db.ProductItems.CountAsync(pi => pi.ProductId.Equals(
+                                                                   productItem.ProductId) &&
+                                                               pi.ItemId.Equals(productItem.ItemId));
+
+            return oldPi.Equals(1) && newPi.Equals(0);
+        }
+
+        public static async Task Change(this ProductItem productItem, ApplicationDbContext db)
+        {
+            var oldProductItem = await db.ProductItems.FirstOrDefaultAsync(
+                pi => pi.ProductId.Equals(productItem.OldProductId) && pi.ItemId.Equals(productItem.OldProductId));
+
+            var newProductItem = await db.ProductItems.FirstOrDefaultAsync(
+                pi => pi.ProductId.Equals(productItem.ProductId) && pi.ItemId.Equals(productItem.ItemId));
+
+            if (oldProductItem != null && newProductItem == null)
+            {
+                newProductItem = new ProductItem
+                {
+                    ItemId = productItem.ItemId,
+                    ProductId = productItem.ProductId
+                };
+
+                using (var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    try
+                    {
+                        db.ProductItems.Remove(oldProductItem);
+                        db.ProductItems.Add(newProductItem);
+
+                        await db.SaveChangesAsync();
+                        transaction.Complete();
+                    }
+                    catch { transaction.Dispose();}
+                }
+            } 
+
+
         }
     }
 }
