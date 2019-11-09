19c26eec1ba1de785c4150643618b3d49394c624
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-09 20:24:42 +0600

    Button shared views and Index of users and subscriptions

--- Memberships/Areas/Admin/Models/SmallButtonModel.cs
+++ Memberships/Areas/Admin/Models/SmallButtonModel.cs
@@ -16,7 +16,7 @@ namespace Memberships.Areas.Admin.Models
         public int? ItemId { get; set; }
         public int? ProductId { get; set; }
         public int? SubscriptionId { get; set; }
-
+        public string UserId { get; set; }
         public string ActionParameters {
             get
             {
@@ -32,6 +32,8 @@ namespace Memberships.Areas.Admin.Models
 
                 if (SubscriptionId != null && SubscriptionId > 0)
                     param.Append(String.Format("{0}={1}&", "subscriptionId", SubscriptionId));
+                if (UserId != null && !UserId.Equals(String.Empty))
+                    param.Append(String.Format("{0}={1}&", "userId", UserId));
 
                 return param.ToString().Substring(0, param.Length - 1);
             } }
