e4bc966dafcb45874eca24b43ac0bc4c75c52836
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-09 17:13:06 +0600

    Restrict access to the admin menus and controllers

--- Memberships/Areas/Admin/Controllers/ProductsController.cs
+++ Memberships/Areas/Admin/Controllers/ProductsController.cs
@@ -14,6 +14,7 @@ using Memberships.Models;
 
 namespace Memberships.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
