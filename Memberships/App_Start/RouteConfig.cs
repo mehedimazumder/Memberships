48de892caff71e66d4d1a27835f68e6d729903dd
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-12 21:40:07 +0600

    Custom route for productcontent/content/../..

--- Memberships/App_Start/RouteConfig.cs
+++ Memberships/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Memberships
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ProductContent",
+                url: "{controller}/{action}/{productId}/{itemId}",
+                defaults: new { controller = "ProductContent", action = "Index" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
