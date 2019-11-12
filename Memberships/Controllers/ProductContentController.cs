34a3bd2bdc51442b449d64b62a847e738b7f9794
mehedi <mehedirumc12cuet14@gmail.com>
2019-11-12 14:42:42 +0600

    Adding ProductContent/Index and section and item models

---
+++ Memberships/Controllers/ProductContentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memberships.Controllers
+{
+    public class ProductContentController : Controller
+    {
+        // GET: ProductContent
+        public async Task<ActionResult> Index(int id)
+        {
+            return View();
+        }
+    }
+}
